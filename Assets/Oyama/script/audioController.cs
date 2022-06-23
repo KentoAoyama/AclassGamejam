1769a93ded017939411e19d41654418fcf165796
Ooyamashun0724 <shun20010724@icloud.com>
2022-06-23 15:22:40 +0900

    音量設定実装

---
+++ Assets/Oyama/script/audioController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class audioController : MonoBehaviour
+{
+    [SerializeField] Slider _slider;
+    [SerializeField] AudioSource _volume;
+    static float _hikitugi;
+    [SerializeField] GameObject _Gslider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _Gslider.SetActive(false);       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _hikitugi = _slider.value;
+        _volume.volume = _hikitugi;       
+    }
+
+    public void Opensettei()
+    {
+        _Gslider.SetActive(true);
+    }
+
+    public void Closesettei()
+    {
+        _Gslider.SetActive(false);
+    }
+}
