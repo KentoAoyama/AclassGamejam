c30abb98d8ce04c8392ddd2341d27eff6ed44bb5
kent\vantan <atoshi1129@gmail.com>
2022-06-23 15:20:22 +0900

    aoyama3

--- Assets/Aoyama/Scripts/Janken.cs
+++ Assets/Aoyama/Scripts/Janken.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Janken : MonoBehaviour
 {
@@ -8,132 +9,120 @@ public class Janken : MonoBehaviour
     [SerializeField] GameObject _1gu;
     [SerializeField] GameObject _1cho;
     [SerializeField] GameObject _1pa;
-    //[SerializeField] GameObject _1right;
-    //[SerializeField] GameObject _1up;
-    //[SerializeField] GameObject _1left;
-    //[SerializeField] GameObject _1down;
     
     
     [SerializeField] GameObject _2gu;
     [SerializeField] GameObject _2cho;
     [SerializeField] GameObject _2pa;
-    //[SerializeField] GameObject _2right;
-    //[SerializeField] GameObject _2up;
-    //[SerializeField] GameObject _2left;
-    //[SerializeField] GameObject _2down;
 
     
-    //Ç†Ç¡ÇøÇﬁÇ¢ÇƒÇŸÇ¢ÉÇÅ[ÉhÇÃämîF
-    public static bool _amh;
 
     //ÇPPÇ∂Ç·ÇÒÇØÇÒ
     bool _1g;
     bool _1c;
     bool _1p;
-    //1PÇ†Ç¡Çø
-    //bool _1L;
-    //bool _1U;
-    //bool _1R;
-    //bool _1D;
 
 
     //ÇQPÇ∂Ç·ÇÒÇØÇÒ
     bool _2g;
     bool _2c;
     bool _2p;
-    //2PÇ†Ç¡Çø
-    //bool _2L;
-    //bool _2U;
-    //bool _2R;
-    //bool _2D;
 
+    bool _1pWin;
+    bool _2pWin;
 
-    //èüîsÇÃîªíf
-    bool _1Pwin;
-    bool _2Pwin;
-
-
-    GameObject _1Pdefault;
-    GameObject _2Pdefault;
-
-    GameObject _1P;
-    GameObject _2P;
-
-    void OnEnbled()
+    bool _1sellected
     {
-        _amh = false;
-        
-        _1P = GameObject.FindWithTag("Player1");
-        _2P = GameObject.FindWithTag("Player2");
-    }
-
-    void FixedUpdate()
-    {
-        if (_amh == false)
+        get 
         {
-            JankenSystem(KeyCode.A, _1g);
-            JankenSystem(KeyCode.S, _1c);
-            JankenSystem(KeyCode.D, _1p);
-
-
-            JankenSystem(KeyCode.LeftArrow, _2g);
-            JankenSystem(KeyCode.DownArrow, _2c);
-            JankenSystem(KeyCode.RightArrow, _2p);
+            return _1g || _1c || _1p;
         }
-        
-        
-        //AmhSystem(KeyCode.A, _1L);
-        //AmhSystem(KeyCode.W, _1U);
-        //AmhSystem(KeyCode.S, _1R);
-        //AmhSystem(KeyCode.D, _1D);
-
-        //AmhSystem(KeyCode.LeftArrow, _2L);
-        //AmhSystem(KeyCode.UpArrow, _2U);
-        //AmhSystem(KeyCode.RightArrow, _2R);
-        //AmhSystem(KeyCode.DownArrow, _2D);
-
-
-        if (_1g == true) _1gu.SetActive(true);
-        if (_1c == true) _1cho.SetActive(true);
-        if (_1p == true) _1pa.SetActive(true);
-        
-        if (_2g == true) _2gu.SetActive(true);
-        if (_2c == true) _2cho.SetActive(true);
-        if (_2p == true) _2pa.SetActive(true);
-
-        if (_1g == false) _1gu.SetActive(false);
-        if (_1c == false) _1cho.SetActive(false);
-        if (_1p == false) _1pa.SetActive(false);
-
-        if (_2g == false) _2gu.SetActive(false);
-        if (_2c == false) _2cho.SetActive(false);
-        if (_2p == false) _2pa.SetActive(false);
     }
 
+    bool _2sellected
+    {
+        get
+        {
+            return _2g || _2c || _2p;
+        }
+    }
+
+
+    void OnEnable()
+    {
+        GameManager._amh = false;
+
+       _1gu.SetActive(false);
+       _1cho.SetActive(false);
+       _1pa.SetActive(false);
+
+       _2gu.SetActive(false);
+       _2cho.SetActive(false);
+       _2pa.SetActive(false);
+    }
 
     void Update()
     {
-        if (_1gu.activeSelf == false || _1cho.activeSelf == false || _1pa.activeSelf == false ||
-            _2gu.activeSelf == false || _2cho.activeSelf == false || _2pa.activeSelf == false)
+        if (!_1sellected)
         {
+            JankenSystem(KeyCode.A, ref _1g);
+            JankenSystem(KeyCode.S, ref _1c);
+            JankenSystem(KeyCode.D, ref _1p);
+        }
+
+
+        if (!_2sellected)
+        {
+            JankenSystem(KeyCode.LeftArrow, ref _2g);
+            JankenSystem(KeyCode.DownArrow, ref _2c);
+            JankenSystem(KeyCode.RightArrow, ref _2p);
+        }
+
+
+        if (_1pWin || _2pWin)
+        {
+            GameManager._amh = true;
+            _1pWin = false;
+            _2pWin = false;
+        }
+
+
+        if (_1sellected && _2sellected)  //óºï˚Ç™âüÇ≥ÇÍÇƒÇΩÇÁ
+        {
+            _1gu.SetActive(_1g);
+            _1cho.SetActive(_1c);
+            _1pa.SetActive(_1p);
+
+            _2gu.SetActive(_2g);
+            _2cho.SetActive(_2c);
+            _2pa.SetActive(_2p);
+        }
+
+  
             if (_1g == true && _2g == true)
             {
                 _1g = false;
                 _2g = false;
+
+                Invoke("DestroyHands", 0.5f);
             }
             if (_1g == true && _2c == true)
             {
                 _1g = false;
                 _2c = false;
 
-                GameManeger._1pWinCount += 1;
+                _1pWin = true;
+
+                Invoke("DestroyHands", 0.5f);
             }
             if (_1g == true && _2p == true)
             {
                 _1g = false;
                 _2p = false;
 
-                GameManeger._2pWinCount += 1;
+                _2pWin = true;
+
+                Invoke("DestroyHands", 0.5f);
             }
             
 
@@ -142,19 +131,24 @@ public class Janken : MonoBehaviour
                 _1c = false;
                 _2g = false;
 
-                GameManeger._2pWinCount += 1;
+                _2pWin = true;
+
+                Invoke("DestroyHands", 0.5f);
             }
             if (_1c == true && _2c == true)
             {
                 _1c = false;
                 _2c = false;
+
+                Invoke("DestroyHands", 0.5f);
             }
             if (_1c == true && _2p == true)
             {
                 _1c = false;
                 _2p = false;
 
-                GameManeger._1pWinCount += 1;
+                _1pWin = true;
+             Invoke("DestroyHands", 0.5f);
             }
 
 
@@ -163,46 +157,47 @@ public class Janken : MonoBehaviour
                 _1p = false;
                 _2g = false;
 
-                GameManeger._1pWinCount += 1;
+                _1pWin = true;
+                
+                Invoke("DestroyHands", 0.5f);
             }
             if (_1p == true && _2c == true)
             {
                 _1p = false;
                 _2c = false;
 
-                GameManeger._2pWinCount += 1;
+                _2pWin = true;
+
+                Invoke("DestroyHands", 0.5f);
             }
             if (_1p == true && _2p == true)
             {
                 _1p = false;
                 _2p = false;
+
+             Invoke("DestroyHands", 0.5f);
             }
-
-        }
     }
 
 
-    void JankenSystem(KeyCode x, bool a)
+    void JankenSystem(KeyCode x,ref bool a)
     {
-        if (Input.GetKeyDown(x) && _amh == false)
-        {
-            Button(a);
-        }
-    }
-
-    void  Button(bool a)
-    {
-        if (a == false)
+        if (Input.GetKeyDown(x) && GameManager._amh == false)
         {
             a = true;
+            Debug.Log("É{É^ÉìÇ™âüÇ≥ÇÍÇΩÇÊ");
         }
     }
-    
-    //void AmhSystem(KeyCode j, bool a)
-    //{
-    //    if (Input.GetKeyDown(j) && _amh == true)
-    //    {
-    //        Button(a);
-    //    }
-    //}
+
+
+    void DestroyHands()
+    {
+        _1gu.SetActive(false);
+        _1cho.SetActive(false);
+        _1pa.SetActive(false);
+
+        _2gu.SetActive(false);
+        _2cho.SetActive(false);
+        _2pa.SetActive(false);
+    }
 }
